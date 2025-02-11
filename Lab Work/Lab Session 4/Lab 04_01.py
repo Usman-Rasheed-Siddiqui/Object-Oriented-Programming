443fd40855831385ea242d19fb15ad1ba3acf116
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2025-02-12 00:10:17 +0500

    Lab Session 6 and 4 uploaded

---
+++ Lab Work/Lab Session 4/Lab 04_01.py
@@ -0,0 +1,41 @@
+
+class Person:
+    def __init__(self, name=''):
+        self.name = name
+
+    def showName(self):
+        print(f"Name: {self.name}")
+
+class Student(Person):
+    def __init__(self, name='', dept='', year=0000):
+        super().__init__(name)
+        self.department = dept
+        self.year = year
+
+    def showName(self):
+        super().showName()
+        print(f"Department: {self.department}")
+        print(f"Year: {self.year}")
+
+
+class Teacher(Person):
+    def __init__(self, name='', course=''):
+        super().__init__(name)
+        self.course = course
+
+    def showName(self):
+        super().showName()
+        print(f"Course: {self.course}")
+
+p1 = Person("Ali")
+p1.showName()
+
+
+p2 = Student("Usman", "CIS Department", 2024)
+p2.showName()
+
+
+p3 = Teacher("Hammad Uddin", "Basic Electrical Engineering")
+p3.showName()
+
+
